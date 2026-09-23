ed57857b3d6f550238ca0c24e795c3a44d5188d1
namannn04 <146120647+namannn04@users.noreply.github.com>
2026-09-23 22:48:48 +0530

    feat(home): compose redesigned homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,27 @@
 import Hero from "@/components/home/Hero";
-import Highlights from "@/components/home/Highlights";
-import Features from "@/components/home/Features";
-import Partners from "@/components/home/Partners";
-import Services from "@/components/home/Services";
+import FeaturedEvents from "@/components/home/FeaturedEvents";
+import Offerings from "@/components/home/Offerings";
+import Impact from "@/components/home/Impact";
+import Story from "@/components/home/Story";
 import Speakers from "@/components/home/Speakers";
+import Partners from "@/components/home/Partners";
+import Voices from "@/components/home/Voices";
 import Faq from "@/components/home/Faq";
+import JoinCta from "@/components/ui/JoinCta";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Highlights />
-      <Features />
-      <Partners />
-      <Services />
+      <FeaturedEvents />
+      <Offerings />
+      <Impact />
+      <Story />
       <Speakers />
+      <Partners />
+      <Voices />
       <Faq />
+      <JoinCta />
     </>
   );
 }
